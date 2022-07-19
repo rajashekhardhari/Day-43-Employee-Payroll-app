e9c111e32664ba509f5a54429330fb768ba65fb2
srajashekhar <udayarajashekhar@gmail.com>
2022-07-19 15:05:16 +0530

    Added: UC-10 for Employee Payroll perform Validation

--- js/EmployeePayroll.js
+++ js/EmployeePayroll.js
@@ -1,6 +1,7 @@
 const salary=document.querySelector('#salary');
 const output =document.querySelector('.salary-output');
 output.textContent=salary.value;
-salary.addEventListener('input',function() {
+salary.addEventListener('input',function() 
+{
 output.textContent=salary.value;
 });
